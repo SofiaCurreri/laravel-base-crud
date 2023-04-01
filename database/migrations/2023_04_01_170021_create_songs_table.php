e50312ac732fe76231399b010818f36772d8c233
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-01 19:16:00 +0200

    model + controller + def tipo variabili tabella

---
+++ database/migrations/2023_04_01_170021_create_songs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('album', 50)->nullable();
+            $table->string('author', 50);
+            $table->string('editor', 50)->nullable();
+            $table->tinyInteger('length')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+};
