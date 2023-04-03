9f1b45c4d007f1832c910d3bcf8033a3bf10c618
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-03 09:28:37 +0200

    added routes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,6 +8,6 @@ use App\Models\Song;
 class PageController extends Controller
 {
     public function index() {
-        return view('home');
+        return view('homepage');
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>{{env('APP_NAME')}}</title>
-
-  {{-- Includiamo gli assets con la direttiva @vite --}}
-  @vite('resources/js/app.js')
-</head>
-
-<body>
-  <main>
-    <h1>Homepage</h1>
-  </main>
-</body>
-
-</html>
+@section('page_name', 'Homepage')
