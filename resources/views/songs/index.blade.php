462db9700503d2dc40e4a320a86ed6db8017b0a9
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-03 17:53:12 +0200

    lista brani stampata

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -3,8 +3,25 @@
 @section('page_name', 'Lista Brani')
     
 @section('main_content')
-    main_content
     <table class="table table-dark table-striped">
-        ...
+        <thead>
+            <tr>
+              <th scope="col">Titolo</th>
+              <th scope="col">Album</th>
+              <th scope="col">Autore</th>
+              <th scope="col">Durata</th>
+            </tr>
+          </thead>
+
+          <tbody>
+              @foreach ($songs as $song)                
+                <tr>
+                    <th scope="row">{{ $song->title }}</th>
+                    <td>{{ $song->album }}</td>
+                    <td>{{ $song->author }}</td>
+                    <td>{{ $song->length }}</td>
+                </tr>
+            @endforeach
+          </tbody>
     </table>
 @endsection
