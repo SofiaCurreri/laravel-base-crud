d9529828276f45d0e4363411048145a88b9ed1b3
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-04 17:55:26 +0200

    value="old()" to keep correct data inserted

--- resources/views/songs/create.blade.php
+++ resources/views/songs/create.blade.php
@@ -19,32 +19,32 @@
 
         <div class="col-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
         </div>
 
         <div class="col-3">
             <label for="album" class="form-label">Album</label>
-            <input type="text" class="form-control" id="album" name="album">
+            <input type="text" class="form-control" id="album" name="album" value="{{old('album')}}">
         </div>
 
         <div class="col-3">
             <label for="author" class="form-label">Autore</label>
-            <input type="text" class="form-control" id="author" name="author">
+            <input type="text" class="form-control" id="author" name="author" value="{{old('author')}}">
         </div>
 
         <div class="col-3">
             <label for="editor" class="form-label">Editore</label>
-            <input type="text" class="form-control" id="editor" name="editor">
+            <input type="text" class="form-control" id="editor" name="editor" value="{{old('editor')}}">
         </div>
 
         <div class="col-3">
             <label for="length" class="form-label">Durata</label>
-            <input type="text" class="form-control" id="length" name="length">
+            <input type="text" class="form-control" id="length" name="length" value="{{old('length')}}">
         </div>
 
         <div class="col-3">
             <label for="poster" class="form-label">Copertina</label>
-            <input type="text" class="form-control" id="poster" name="poster">
+            <input type="text" class="form-control" id="poster" name="poster" value="{{old('poster')}}">
         </div>
 
         <div class="col-12 d-flex">
