28392c881225529279ee4ad324239f29e25581c9
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-04 18:56:57 +0200

    building update()

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -116,7 +116,7 @@ class SongController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Song $song)
     {
         //
     }
--- resources/views/songs/edit.blade.php
+++ resources/views/songs/edit.blade.php
@@ -16,10 +16,11 @@
 
     <form action="{{ route('songs.update', $song) }}" method="POST" class="row gy-3 gx-5 text-light">
         @csrf
+        @method('PUT')
 
         <div class="col-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title') ?? $song->title}}">
             <div class="invalid-feedback">
                 Il titolo è obbligatorio
             </div>
@@ -27,7 +28,7 @@
 
         <div class="col-3">
             <label for="album" class="form-label">Album</label>
-            <input type="text" class="form-control @error('album') is-invalid @enderror" id="album" name="album" value="{{old('album')}}">
+            <input type="text" class="form-control @error('album') is-invalid @enderror" id="album" name="album" value="{{old('album') ?? $song->album}}">
             <div class="invalid-feedback">
                 L' album deve essere una stringa
             </div>
@@ -35,7 +36,7 @@
 
         <div class="col-3">
             <label for="author" class="form-label">Autore</label>
-            <input type="text" class="form-control @error('author') is-invalid @enderror" id="author" name="author" value="{{old('author')}}">
+            <input type="text" class="form-control @error('author') is-invalid @enderror" id="author" name="author" value="{{old('author') ?? $song->author}}">
             <div class="invalid-feedback">
                 L' autore è obbligatorio
             </div>
@@ -43,7 +44,7 @@
 
         <div class="col-3">
             <label for="editor" class="form-label">Editore</label>
-            <input type="text" class="form-control @error('editor') is-invalid @enderror" id="editor" name="editor" value="{{old('editor')}}">
+            <input type="text" class="form-control @error('editor') is-invalid @enderror" id="editor" name="editor" value="{{old('editor') ?? $song->editor}}">
             <div class="invalid-feedback">
                 L' editor deve essere una stringa
             </div>
@@ -51,7 +52,7 @@
 
         <div class="col-3">
             <label for="length" class="form-label">Durata</label>
-            <input type="text" class="form-control @error('length') is-invalid @enderror" id="length" name="length" value="{{old('length')}}">
+            <input type="text" class="form-control @error('length') is-invalid @enderror" id="length" name="length" value="{{old('length') ?? $song->length}}">
             <div class="invalid-feedback">
                 Durata di tipo 00:00:00
             </div>
@@ -59,7 +60,7 @@
 
         <div class="col-3">
             <label for="poster" class="form-label">Copertina</label>
-            <input type="text" class="form-control @error('poster') is-invalid @enderror" id="poster" name="poster" value="{{old('poster')}}">
+            <input type="text" class="form-control @error('poster') is-invalid @enderror" id="poster" name="poster" value="{{old('poster') ?? $song->poster}}">
             <div class="invalid-feedback">
                 La copertina deve essere una stringa
             </div>
